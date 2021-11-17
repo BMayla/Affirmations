d3bec1f0720a3c142d2e174d7db3fa18af64f055
BMayla <maylabouzakoura@gmail.com>
2021-11-17 11:14:40 +0100

    Initial commit

---
+++ app/src/main/java/com/maylabouzakoura/affirmations/data/Datasource.kt
@@ -0,0 +1,23 @@
+package com.maylabouzakoura.affirmations.data
+
+import com.maylabouzakoura.affirmations.R
+import com.maylabouzakoura.affirmations.model.Affirmation
+
+
+class Datasource {
+
+    fun loadAffirmations(): List<Affirmation> {
+        return listOf<Affirmation>(
+            Affirmation(R.string.affirmation1),
+            Affirmation(R.string.affirmation2),
+            Affirmation(R.string.affirmation3),
+            Affirmation(R.string.affirmation4),
+            Affirmation(R.string.affirmation5),
+            Affirmation(R.string.affirmation6),
+            Affirmation(R.string.affirmation7),
+            Affirmation(R.string.affirmation8),
+            Affirmation(R.string.affirmation9),
+            Affirmation(R.string.affirmation10)
+        )
+    }
+}
---
+++ app/src/main/java/com/maylabouzakoura/affirmations/model/Affirmation.kt
@@ -0,0 +1,3 @@
+package com.maylabouzakoura.affirmations.model
+
+data class Affirmation(val stringResourceId: Int)
